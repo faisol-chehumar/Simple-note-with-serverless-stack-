48e7ebd60d56131eb5d44e954c232720fddcaf22
Sol <faisol.chehumar@gmail.com>
2019-04-05 23:36:32 +0700

    Add AWS amplify config

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads-sol"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://il7jmf8cz4.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_5quqWQYfi",
+    APP_CLIENT_ID: "a3mvr9cd0jnknto3lk8g41agk",
+    IDENTITY_POOL_ID: "22a075dd-1c0d-4cd8-ac4f-444eb9a82a7c "
+  }
+}
