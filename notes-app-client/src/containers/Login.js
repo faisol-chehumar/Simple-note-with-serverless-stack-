4bd71c29922d2066ccd3d3d4afe4245d7d840007
Sol <faisol.chehumar@gmail.com>
2019-04-08 23:26:58 +0700

    Add login and logout page

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react"
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap"
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css"
+import { Auth } from "aws-amplify"
 
 export default class Login extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      isLoading: false,
       email: "",
       password: ""
     }
@@ -22,9 +25,21 @@ export default class Login extends Component {
     })
   }
 
-  handleSubmit = event => {
-    event.preventDefault()
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ isLoading: true });
+
+    try {
+      await Auth.signIn(this.state.email, this.state.password);
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
   }
+
 
   render() {
     return (
@@ -47,14 +62,15 @@ export default class Login extends Component {
               type="password"
             />
           </FormGroup>
-          <Button
+          <LoaderButton
             block
             bsSize="large"
             disabled={!this.validateForm()}
             type="submit"
-          >
-            Login
-          </Button>
+            isLoading={this.state.isLoading}
+            text="Login"
+            loadingText="Logging in…"
+          />
         </form>
       </div>
     )
